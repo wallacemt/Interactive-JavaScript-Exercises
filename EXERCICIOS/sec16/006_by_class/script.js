504f0e51c3ef38df607ac9e58ebd5830dcb62e4f
wallacemt <138271229+wallacemt@users.noreply.github.com>
2024-04-09 20:30:54 -0300

    aula sobre DOM

---
+++ EXERCICIOS/sec16/006_by_class/script.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByTagName("li"));
+
+console.log(document.getElementsByClassName("itens-azul"))
+
+console.log(document.getElementsByClassName("itens-vermelhos"))
+
+console.log(document.getElementsByClassName("itens"))
